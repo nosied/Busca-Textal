a2da51f2436dc32dce5774ab9f9400a9b8865c3e
nosied <deisonribeiro14@gmail.com>
2021-09-08 13:08:36 -0300

    Criação do arquivo de busca.js

---
+++ busca.js
@@ -0,0 +1,30 @@
+$( "body" ).prepend("<input type='text' id='term_to_search' placeholder='Termo a ser pesquisado'><button id='start_search'>Gravar e Iniciar</button>");
+$('body').prepend('<audio id="notificacao" preload="auto"><source src="https://vaap.deisonribeiro.com/view/admin/sms-alert-1-daniel_simon.mp3" type="audio/mpeg"></audio>');
+    $('body').on('click', '#start_search',function () {
+        if($('#term_to_search').val() !== null){
+            localStorage.termo = $('#term_to_search').val();
+            //location.reload();
+        }
+    });
+if(localStorage.termo !== null){
+    $('#term_to_search').val(localStorage.termo);
+    setInterval(function(){
+        buscaTermo();
+    }, 3000);
+}
+function tiraAcentos(i) {
+    return i;
+  }
+function buscaTermo(){
+  var searchTerm = localStorage.termo;
+  var html_limpo = $("html").text().trim();
+
+  var pattern = "([^\\w]*)(" + tiraAcentos(searchTerm) + "|" + searchTerm + ")([^\\w]*)";
+  var rg = new RegExp(pattern, "g");
+  var match = rg.exec(html_limpo);
+  if (match) {
+    //console.log(match.length);
+    $('#notificacao').trigger('play');
+    console.log("Termo "+searchTerm+" encontrado!");
+  }
+}
